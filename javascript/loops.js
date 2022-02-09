7fbdb534f94a2560b870bec5285a63c4ca390a1a
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-02-09 17:07:11 -0500

    loops

--- javascript/loops.js
+++ javascript/loops.js
@@ -27,7 +27,7 @@ while (num <=50 ){
     }
     num++;
 }
-console.log('THE LOOP HAS ENDED!');*/
+console.log('THE LOOP HAS ENDED!');
 
 let fullNameEnter = false;
 let name = "";
@@ -37,4 +37,45 @@ while (!fullNameEnter) {
     fullNameEnter = confirm("Is this your full name:" + name);
 
 }
-alert("Your full name is " + name);
+alert("Your full name is: " + name + "!");
+
+
+
+// // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that there are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+//
+let parkingSpace = 30;
+
+do{
+    console.log("This is the number of parking spot available " + parkingSpace);
+    parkinsSpace -= 2;
+} while(parkingSpace)
+console.log("The parking garage is full")
+
+
+
+//TODO: using prompt() and alert(), write a while loop that will continue to ask for the correct password until the
+//  correct password is given.
+
+while(){
+
+    let passWord = "passw0rd123";
+    let isCorrect = false;
+    let attempt = "";
+    while (!isCorrect) {
+
+attempt = prompt("Please enter your password...")
+isCorrect = (attempt === passWord);
+
+if (!isCorrect){
+    alert("that was not correct")
+                }
+
+                        }
+        }
+
+console.log("Correct passWord is given!")
+
+ */
